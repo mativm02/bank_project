f437ec6d14ae33bbf12548503a14b786b0dac2e9
mativm02 <matipvp02@gmail.com>
2023-04-03 23:20:39 -0300

    creating transfer tx

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,18 +15,20 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 // TestMain is the entry point for all tests.
 // It sets up the database connection and runs the tests.
 // It also tears down the database connection after all tests are done.
 // This function is called by the testing package automatically.
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
---
+++ db/sqlc/store.go
@@ -0,0 +1,83 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store provides all functions to execute database queries.
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new store.
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		Queries: New(db),
+		db:      db,
+	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxResults struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// TransferTx performs a transfer from one account to another.
+// It creates a transfer record and updates account balances within a database transaction.
+func (store *Store) TransferTx(ctx context.Context, arg CreateTransferParams) (TransferTxResults, error) {
+	var result TransferTxResults
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, arg)
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: update account balances
+
+		return nil
+	})
+
+	return result, err
+}
